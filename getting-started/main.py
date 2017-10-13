628137751ec0ac5f53cfcdb3865a090f23ad2ef1
georgezhyuan <yuangeorge9981@gmail.com>
2017-10-12 22:23:53 -0600

    added a tensorflow cost function

--- getting-started/main.py
+++ getting-started/main.py
@@ -46,6 +46,24 @@ def variables():
 
     print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
 
+
+def loss_function():
+    W = tf.Variable([.3], dtype=tf.float32)
+    b = tf.Variable([-.3], dtype=tf.float32)
+    x = tf.placeholder(tf.float32)
+    y = tf.placeholder(tf.float32)
+
+    linear_model = W * x + b
+    squared_deltas = tf.square(linear_model - y)
+    loss = tf.reduce_sum(squared_deltas)
+
+    init = tf.global_variables_initializer()
+    sess.run(init)
+
+    print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+
+
 # constants()
 # placeholders()
-variables()
+# variables()
+loss_function()
